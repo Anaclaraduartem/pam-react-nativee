e670319c90c5ca5b19ae130bac4c8ca9e2a00c8d
Anaclaraduartem <anaclaraduartemiranda03@gmail.com>
2023-08-04 12:19:55 -0300

    react native
    fizemos o começo

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Bem Vindos ao </Text>
+      <Text>App da Ana</Text>
       <StatusBar style="auto" />
     </View>
   );
